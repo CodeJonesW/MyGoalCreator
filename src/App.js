76e3dd9acaad78f2b1d9e403ca7804239d415637
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-10-11 16:43:46 -0500

    more cleanup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 import { Box } from "@mui/material";
 import "./App.css";
 import { Analyze, NavBar } from "./components/index.js";
-import { getProfile } from "./redux/slices/profileSlice.js";
-import { clearAuthToken, getAuthToken } from "./redux/slices/authSlice.js";
 import { useTheme } from "@mui/material/styles";
 
 const App = () => {
   const theme = useTheme();
-  const dispatch = useDispatch();
-  const { token } = useSelector((state) => state.authSlice);
-  const { user, goals } = useSelector((state) => state.profileSlice);
-  const [showGoals, setShowGoals] = useState(false);
-
-  useEffect(() => {
-    dispatch(getAuthToken());
-  }, [dispatch]);
 
   return (
     <Box sx={{ width: "100%", height: "100%" }}>
