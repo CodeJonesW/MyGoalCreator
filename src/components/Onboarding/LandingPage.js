f08f5b6e6d7cb4282788b1c76f2d1adede2582fd
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-10-22 10:32:23 -0500

    fix linting

--- src/components/Onboarding/LandingPage.js
+++ src/components/Onboarding/LandingPage.js
@@ -1,18 +1,6 @@
 import React from "react";
-import {
-  Button,
-  Box,
-  Grid,
-  Typography,
-  Paper,
-  Container,
-  List,
-  ListItem,
-  ListItemText,
-} from "@mui/material";
+import { Box } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import GoalIllustration from "../../assets/images/my_goal_creator_landing_page_img_1.webp";
-import { useNavigate } from "react-router-dom";
 import NavBar from "../NavBar";
 import FeaturesSection from "./FeaturesSection";
 import HeroSection from "./HeroSection";
@@ -23,7 +11,6 @@ import Footer from "./Footer";
 
 const LandingPage = () => {
   const theme = useTheme();
-  const navigate = useNavigate();
 
   return (
     <Box className="landing-page">
