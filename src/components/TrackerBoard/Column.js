29c6ef10c59bd006923f24b138cf923afe6ba016
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-10-24 22:30:38 -0500

    swipable columns

--- src/components/TrackerBoard/Column.js
+++ src/components/TrackerBoard/Column.js
@@ -1,31 +1,55 @@
+// Column.js
 import React from "react";
 import { useDroppable } from "@dnd-kit/core";
 import { Task } from "./Task";
 import { useTheme } from "@mui/material/styles";
+import { Box, Typography } from "@mui/material";
+import { motion, AnimatePresence } from "framer-motion";
 
-export const Column = ({ column }) => {
+export const Column = ({ column, isVisible, currentColumn, index }) => {
   const theme = useTheme();
   const { setNodeRef } = useDroppable({
     id: column.id,
   });
-  console.log("COLUMN", column);
+
+  // Animation variants for fade-in and fade-out effects
+  const variants = {
+    hidden: { opacity: 0, x: index < currentColumn ? -100 : 100 },
+    visible: { opacity: 1, x: 0 },
+    exit: { opacity: 0, x: index < currentColumn ? 100 : -100 },
+  };
+
   return (
-    <div
-      ref={setNodeRef}
-      style={{
-        margin: "0 10px",
-        padding: "10px",
-        border: "1px solid #ccc",
-        width: "200px",
-        borderRadius: "16px",
-        backgroundColor: theme.palette.primary.main,
-        color: theme.palette.primary.contrastText,
-      }}
-    >
-      <h2>{column.title}</h2>
-      {column.tasks.map((task) => (
-        <Task key={task.id} task={task} columnId={column.id} />
-      ))}
-    </div>
+    <AnimatePresence>
+      {isVisible && (
+        <motion.div
+          ref={setNodeRef}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          variants={variants}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+          style={{
+            margin: "0 10px",
+            padding: "10px",
+            border: "1px solid #ccc",
+            width: "300px",
+            borderRadius: "16px",
+            backgroundColor: theme.palette.primary.main,
+            color: theme.palette.primary.contrastText,
+            scrollSnapAlign: "center", // Ensures the column snaps into view
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h6" gutterBottom>
+            {column.title}
+          </Typography>
+          {column.tasks.map((task) => (
+            <Task key={task.id} task={task} columnId={column.id} />
+          ))}
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
