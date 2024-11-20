8191406e65b641803fb3fc2a01e013b3f5b7672e
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-11-20 13:42:36 -0600

    use dayjs plugins

--- src/redux/slices/profileSlice.js
+++ src/redux/slices/profileSlice.js
@@ -6,6 +6,8 @@ import axios from "axios";
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export const getProfile = createAsyncThunk(
   "profile/getProfile",
